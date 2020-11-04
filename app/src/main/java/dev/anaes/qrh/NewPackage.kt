2da3fca76a59a36560042bceb43c1ea8f8f30a3a
mttrnd <mattroundphone@gmail.com>
2020-11-04 22:22:42 +0000

    Message screen for old com.mttrnd.qrh package

---
+++ app/src/main/java/dev/anaes/qrh/NewPackage.kt
@@ -0,0 +1,35 @@
+package dev.anaes.qrh
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_about.*
+import kotlinx.android.synthetic.main.activity_newpackage.*
+
+
+class NewPackage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Setup view
+        setContentView(R.layout.activity_newpackage)
+
+        //CC license link
+        GooglePlay.setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=dev.anaes.qrh/")))
+        }
+
+        buttonOld.setOnClickListener {
+            this.startActivity(Intent(this,Splash::class.java))
+            finish()
+        }
+
+    }
+
+    //Close activity on back / up navigation
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+}
