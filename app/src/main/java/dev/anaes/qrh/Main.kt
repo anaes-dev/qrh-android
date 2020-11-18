b3df02384fa161109a9efbf6d58f0726cf1d5492
mttrnd <mattroundphone@gmail.com>
2020-11-18 17:44:30 +0000

    Breadcrumb with fragment navigation

--- app/src/main/java/dev/anaes/qrh/Main.kt
+++ app/src/main/java/dev/anaes/qrh/Main.kt
@@ -1,25 +1,38 @@
 package dev.anaes.qrh
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
+interface PopDetail {
+    fun popToDetail(num: Int)
+}
 
-class Main : AppCompatActivity() {
-
-    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+class Main : AppCompatActivity(), PopDetail {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-//
-//        val toolbar: Toolbar = findViewById(R.id.toolbar)
-//        setSupportActionBar(toolbar)
+        val appBar: AppBarLayout = findViewById(R.id.app_bar)
+        val toolbarLayout: CollapsingToolbarLayout = findViewById(R.id.toolbar_layout)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
 
         val appBarConfiguration = AppBarConfiguration(navController.graph)
 
@@ -27,9 +40,40 @@ class Main : AppCompatActivity() {
 
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        var count = navController.backStack.count()
+
+        if(breadcrumbIsActive) {
+            breadcrumbCount--
+            breadcrumbList.removeLast()
+        }
+
         findNavController(R.id.nav_host_fragment).navigateUp()
         return true
     }
+
+    override fun popToDetail(num: Int) {
+        var x = num
+        Log.d("test", num.toString())
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        while (x > 0) {
+            navController.popBackStack()
+            breadcrumbCount--
+            breadcrumbList.removeLast()
+            x--
+        }
+    }
+
+    companion object {
+        var breadcrumbList: ArrayList<String> = ArrayList()
+        var breadcrumbCount: Int = 0
+        var breadcrumbIsActive: Boolean = false
+    }
+
 
 }
--- app/src/main/java/dev/anaes/qrh/ui/main/DetailFragment.kt
+++ app/src/main/java/dev/anaes/qrh/ui/main/DetailFragment.kt
@@ -1,21 +1,20 @@
 package dev.anaes.qrh.ui.main
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.doOnPreDraw
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavOptions
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
-import dev.anaes.qrh.CardRecyclerAdapter
-import dev.anaes.qrh.DetailContent
-import dev.anaes.qrh.R
+import dev.anaes.qrh.*
 import kotlinx.android.synthetic.main.activity_detail_scrollingcontent.*
 
 
@@ -31,51 +30,164 @@ class DetailFragment : Fragment(), PushDetail {
 
     private lateinit var linearLayoutManager: LinearLayoutManager
 
+    private var getDetails: Array<String> = arrayOf()
+    var title: String? = String()
+    var version: String? = String()
+    var code: String? = String()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        code = args.code
+        getDetails = getDetailsFromCode(code)
+        title = getDetails[0]
+        version = getDetails[2]
+
+
+
+        activity?.findViewById<AppBarLayout>(R.id.app_bar)?.setExpanded(true)
+        activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = title
+
+        Main.breadcrumbList.add(title.toString())
+        Main.breadcrumbCount++
+        Main.breadcrumbIsActive = true
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        setHasOptionsMenu(true)
-//        activity?.findViewById<AppBarLayout>(R.id.app_bar)?.setExpanded(true)
-//        activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = "test"
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        postponeEnterTransition()
+
+
+        activity.let { it ->
+            if (it != null) {
+                it.findViewById<TextView>(R.id.detail_code).text = code
+                it.findViewById<TextView>(R.id.detail_code_2).text = code
+                it.findViewById<TextView>(R.id.detail_version).text = version
+            }
+        }
+
 
         val filenameSuffix = ".json"
-        val filename = args.code + filenameSuffix
+        val filename = code + filenameSuffix
 
-        val content = DetailContent.getContentFromFile(
-            filename,
-            context
-        )
+        this.context?.let { safeContext ->
+            val content = DetailContent.getContentFromFile(
+                filename,
+                safeContext
+            )
+            val adapter =
+                CardRecyclerAdapter(content, code) { code: String ->
+                    navToDetail(code)
+                }
+            linearLayoutManager = LinearLayoutManager(context)
+            detail_recyclerview.layoutManager = linearLayoutManager
+            detail_recyclerview.isNestedScrollingEnabled = false
+            detail_recyclerview.adapter = adapter
+        } ?: run {
+            Error("Internal Error")
+        }
 
-        val adapter =
-            CardRecyclerAdapter(content, args.code) { code: String ->
-                navToDetail(code)
+        val bcStack = detail_stack
+
+        if(Main.breadcrumbCount > 1) {
+            detail_scroll.isVisible = true
+
+            var bci: Int = Main.breadcrumbCount
+            bci--
+
+
+            for (bc in Main.breadcrumbList) {
+                Log.d("loop", bci.toString())
+                val chevron = ImageView(context)
+                chevron.setImageResource(R.drawable.ic_chevron_right)
+                chevron.maxHeight = 12
+                chevron.maxWidth = 12
+                bcStack.addView(chevron)
+
+                val button = Button(context, null, android.R.attr.buttonBarButtonStyle)
+                button.textSize = 12F
+                button.isAllCaps = false
+                button.text = bc
+                button.tag = bci
+                button.setOnClickListener {
+                    (activity as PopDetail).popToDetail(button.tag as Int)
+                }
+                bcStack.addView(button)
+                bci--
+            }
+            detail_home.setOnClickListener {
+                Main.breadcrumbCount == 0
+                Main.breadcrumbList.clear()
+                val action = DetailFragmentDirections.PopHome()
+                findNavController().navigate(action)
             }
 
-        linearLayoutManager = LinearLayoutManager(context)
-        detail_recyclerview.layoutManager = linearLayoutManager
-        detail_recyclerview.isNestedScrollingEnabled = false
-        detail_recyclerview.adapter = adapter
+        }
+
+
+        (view.parent as? ViewGroup)?.doOnPreDraw {
+            startPostponedEnterTransition()
+        }
+
     }
 
-//
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        inflater.inflate(R.menu.menu_detail, menu)
-//        super.onCreateOptionsMenu(menu, inflater)
-//    }
+    override fun onResume() {
+        super.onResume()
+        Main.breadcrumbIsActive = true
+        activity?.findViewById<AppBarLayout>(R.id.app_bar)?.setExpanded(true)
+        activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = title
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_detail, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 
     override fun navToDetail(code: String) {
         val action = DetailFragmentDirections.LoadNewDetail(code)
-        findNavController().navigate(action)
+        navController.navigate(action)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.navigation_download -> {
+                val action = DetailFragmentDirections.detailFrag()
+                findNavController().navigate(action)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    private fun getDetailsFromCode(codePassed: String?): Array<String> {
+        this.context?.let { safeContext ->
+            val guideList = Guideline.getGuidelinesFromFile(
+                "guidelines.json",
+                safeContext
+            )
+            val position: Int = guideList.indexOfFirst { it.code == codePassed }
+            return arrayOf(
+                guideList[position].title,
+                guideList[position].url,
+                guideList[position].version.toString()
+            )
+        } ?: run {
+            return arrayOf(
+                "Default", "", ""
+            )
+        }
+    }
+
 }
 
