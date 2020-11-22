6d835639c40ed5fb3af5f81b3caaf2bfb81b5f82
anesdev <anaesdev@gmail.com>
2020-11-22 15:18:17 +0000

    Tidy up

--- app/src/main/java/dev/anaes/qrh/Main.kt
+++ app/src/main/java/dev/anaes/qrh/Main.kt
@@ -9,7 +9,6 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import android.view.inputmethod.InputMethodManager
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
